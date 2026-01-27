c18493fd3d08448115829b8f29712aaf18b1cfc4
Valiares <llagasjahaziel@gmail.com>
2026-01-28 05:14:19 +0800

    fixed the link where it wont work

--- src/lib/components/signUp.svelte
+++ src/lib/components/signUp.svelte
@@ -125,7 +125,7 @@
 
 							<div class="space-y-3 pt-4">
 								<a
-									href="https://expo.dev/artifacts/eas/uYYPu9uqcmphg7XyLJkdsc.apk"
+									href="https://expo.dev/artifacts/eas/kia5uLtgR1zGuFAYrwAoiC.apk"
 									class="block w-full rounded-lg bg-gradient-to-r from-[#B4A063] to-[#DEDAA0] py-3 text-center text-base font-bold text-gray-900 transition-colors hover:bg-[#d9bc5f] md:py-4 md:text-lg"
 								>
 									Download
